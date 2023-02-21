c916dfa5ad1ac0990d1c58fc9acfe0cf12e4c804
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-02-21 09:10:27 -0800

    Add:Welcome to the Cab INVOICE GENERATOR

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the cab invoice generator" );
+    }
+}
